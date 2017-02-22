22d41a858f049b3eb5d8bdc6f566aeefd2c6fac0
Sumit Asok <sumitasok@gmail.com>
2017-02-22 23:36:04 +0530

    finding the colored lane using triangle

--- runner.py
+++ runner.py
@@ -115,55 +115,49 @@ def process_image(image):
     # TODO: put your pipeline here,
     # you should return the final output (image where lines are drawn on lanes)
 
-    # Grab the x and y size and make a copy of the image
+    # Grab the x and y sizes and make two copies of the image
+    # With one copy we'll extract only the pixels that meet our selection,
+    # then we'll paint those pixels red in the original image to see our selection 
+    # overlaid on the original.
     ysize = image.shape[0]
     xsize = image.shape[1]
-    # Note: always make a copy rather than simply using "="
-    region_select = np.copy(image)
+    color_select= np.copy(image)
+    line_image = np.copy(image)
 
+    # Define our color criteria
+    red_threshold = 200
+    green_threshold = 200
+    blue_threshold = 200
+    rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
-    # Define a triangle region of interest
+    # Define a triangle region of interest (Note: if you run this code, 
     # Keep in mind the origin (x=0, y=0) is in the upper left in image processing
-    # Note: if you run this code, you'll find these are not sensible values!!
-    # But you'll get a chance to play with them soon in a quiz
-    left_bottom = [0, 539]
-    right_bottom = [900, 300]
-    apex = [400, 0]
+    # you'll find these are not sensible values!!
+    # But you'll get a chance to play with them soon in a quiz ;)
+    left_bottom = [120, 539]
+    right_bottom = [850, 539]
+    apex = [490, 220]
 
-    # Fit lines (y=Ax+B) to identify the  3 sided region of interest
-    # np.polyfit() returns the coefficients [A, B] of the fit
     fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
     fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
     fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+    # Mask pixels below the threshold
+    color_thresholds = (image[:,:,0] < rgb_threshold[0]) | \
+                        (image[:,:,1] < rgb_threshold[1]) | \
+                        (image[:,:,2] < rgb_threshold[2])
 
     # Find the region inside the lines
     XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
     region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
                         (YY > (XX*fit_right[0] + fit_right[1])) & \
                         (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+    # Mask color selection
+    color_select[color_thresholds] = [0,0,0]
+    # Find where image is both colored right and in the region
+    line_image[~color_thresholds & region_thresholds] = [255,0,0]
 
-    # Color pixels red which are inside the region of interest
-    region_select[region_thresholds] = [255, 0, 0]
-
-    # Define our color selection criteria
-    # Note: if you run this code, you'll find these are not sensible values!!
-    # But you'll get a chance to play with them soon in a quiz
-    red_threshold = 200
-    green_threshold = 200
-    blue_threshold = 200
-    rgb_threshold = [red_threshold, green_threshold, blue_threshold]
-
-    # Identify pixels below the threshold
-    thresholds = (image[:,:,0] < rgb_threshold[0]) \
-                | (image[:,:,1] < rgb_threshold[1]) \
-                | (image[:,:,2] < rgb_threshold[2])
-    region_select[thresholds] = [0,0,0]
-
-    # Display the image               
-    # plt.imshow(region_select)
-    # plt.show()
-
-    return region_select
+    return line_image
 
 white_output = 'white.mp4'
 clip1 = VideoFileClip("solidWhiteRight.mp4")
