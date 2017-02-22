8b4fe7610712b7d59ca7b72d24428c2ae15b5312
Sumit Asok <sumitasok@gmail.com>
2017-02-22 23:05:02 +0530

    add color red to identified places

--- runner.py
+++ runner.py
@@ -119,7 +119,31 @@ def process_image(image):
     ysize = image.shape[0]
     xsize = image.shape[1]
     # Note: always make a copy rather than simply using "="
-    color_select = np.copy(image)
+    region_select = np.copy(image)
+
+
+    # Define a triangle region of interest
+    # Keep in mind the origin (x=0, y=0) is in the upper left in image processing
+    # Note: if you run this code, you'll find these are not sensible values!!
+    # But you'll get a chance to play with them soon in a quiz
+    left_bottom = [0, 539]
+    right_bottom = [900, 300]
+    apex = [400, 0]
+
+    # Fit lines (y=Ax+B) to identify the  3 sided region of interest
+    # np.polyfit() returns the coefficients [A, B] of the fit
+    fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+    fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+    fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+    # Find the region inside the lines
+    XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+    region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
+                        (YY > (XX*fit_right[0] + fit_right[1])) & \
+                        (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+
+    # Color pixels red which are inside the region of interest
+    region_select[region_thresholds] = [255, 0, 0]
 
     # Define our color selection criteria
     # Note: if you run this code, you'll find these are not sensible values!!
@@ -133,13 +157,13 @@ def process_image(image):
     thresholds = (image[:,:,0] < rgb_threshold[0]) \
                 | (image[:,:,1] < rgb_threshold[1]) \
                 | (image[:,:,2] < rgb_threshold[2])
-    color_select[thresholds] = [0,0,0]
+    region_select[thresholds] = [0,0,0]
 
     # Display the image               
-    # plt.imshow(color_select)
+    # plt.imshow(region_select)
     # plt.show()
 
-    return color_select
+    return region_select
 
 white_output = 'white.mp4'
 clip1 = VideoFileClip("solidWhiteRight.mp4")
